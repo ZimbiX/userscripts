b0feac4cf858ed1e6d73062c5f0d5c0766fc45ba
Brendan Weibrecht <brendan@weibrecht.net.au>
2024-11-04 14:28:12 +1100

    Make Buildkite logs userscript run for any org

--- js/Buildkite: Expand logs sections automatically.user.js
+++ js/Buildkite: Expand logs sections automatically.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Brendan Weibrecht
-// @match        https://buildkite.com/gs/*/builds/*
+// @match        https://buildkite.com/*/*/builds/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=buildkite.com
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/ZimbiX/userscripts/master/js/Buildkite%3A%20Expand%20logs%20sections%20automatically.user.js
