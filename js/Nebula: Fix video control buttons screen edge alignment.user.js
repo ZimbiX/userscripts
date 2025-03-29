21fffb542ea147e7a233d1c24796d0db9150c719
Brendan Weibrecht <brendan@weibrecht.net.au>
2025-03-30 05:04:35 +1100

    Nebula: Update for new selectors

--- js/Nebula: Fix video control buttons screen edge alignment.user.js
+++ js/Nebula: Fix video control buttons screen edge alignment.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nebula: Fix video control buttons screen edge alignment
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.2
 // @description  Make it easier to click on the play/pause and fullscreen buttons - being able to just flick your cursor to the corners of the screen, like with YouTube's fullscreen button
 // @author       You
 // @match        https://nebula.app/*
@@ -33,17 +33,17 @@
             "}",
 
             // Restore the original position of the leftmost button while making the clickable area larger, by adding padding
-            "#toggle-play-button, #replay-button {",
+            '#video-controls button[aria-label="Play"], #video-controls button[aria-label="Pause"], #video-controls button[aria-label="Replay"] {',
             "  box-sizing: content-box !important;",
             "  padding-left: 24px !important;",
             "}",
 
             // Restore the original position of the rightmost button while making the clickable area larger, by adding padding.
             // The padding goes inside the SVG so the icon highlight hover area is increased too
-            "#fullscreen-mode-button {",
+            '#video-controls button[aria-label="Full screen"] {',
             "  width: calc(33px + 24px) !important;",
             "}",
-            "#fullscreen-mode-button > svg {",
+            '#video-controls button[aria-label="Full screen"] > svg {',
             "  width: calc(32px + 24px) !important;",
             "  padding-right: 24px !important;",
             "}",
@@ -56,6 +56,11 @@
             // Prevent the stupid curve overlays on the video corners from blocking corner clicks by bringing the controls in front
             "#video-player .video-player-controls-wrapper {",
             "  z-index: 3 !important;",
+            "}",
+
+            // There is an empty div after the fullscreen button that results in superfluous spacing. Get rid of it
+            '#video-controls button[aria-label="Full screen"] + div {',
+            "  position: absolute !important;",
             "}",
         ].join(' ')
     )
