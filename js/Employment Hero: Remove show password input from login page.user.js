daa7218a4b61b57b4a9119106bc217b0e34bacfe
Brendan Weibrecht <brendan@weibrecht.net.au>
2025-05-07 15:33:23 +1000

    Add script: Employment Hero: Remove show password input from login page

---
+++ js/Employment Hero: Remove show password input from login page.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Employment Hero: Remove show password input from login page
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-07
+// @description  The checkbox is deceptive, given a login checkbox is normally for 'remember me'
+// @author       You
+// @match        https://secure.employmenthero.com/users/sign_in
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=employmenthero.com
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/ZimbiX/userscripts/master/js/Employment%20Hero%3A%20Remove%20show%20password%20input%20from%20login%20page.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const run = (showPasswordFieldLabel) => {
+        showPasswordFieldLabel.style.display = 'none';
+        showPasswordFieldLabel.parentNode.style.justifyContent = 'center';
+    };
+
+    const runWhenReady = () => {
+        const showPasswordFieldLabel = Array.from(document.querySelectorAll('label')).filter(e => e.textContent === 'Show password')[0];
+        if (showPasswordFieldLabel) {
+            run(showPasswordFieldLabel);
+        } else {
+            setTimeout(runWhenReady, 5);
+        }
+    };
+
+    runWhenReady();
+})();
