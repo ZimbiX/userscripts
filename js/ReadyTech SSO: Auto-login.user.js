e1d7ab7197c4a164523fb9bb0dd15b5233049dbb
Brendan Weibrecht <brendan@weibrecht.net.au>
2025-05-20 21:30:35 +1000

    Fix icon for ReadyTech SSO auto-login script
    Despite the script no longer being useful to me

--- js/ReadyTech SSO: Auto-login.user.js
+++ js/ReadyTech SSO: Auto-login.user.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://readytech-prod.au.auth0.com/u/login/identifier?*
-// @icon         https://www.readytech.com.au/_resources/themes/readytech/src/img/favicon.ico
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=readytech.com.au
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/ZimbiX/userscripts/master/js/ReadyTech%20SSO%3A%20Auto-login.user.js
 // ==/UserScript==
